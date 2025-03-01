95790f495687e924dd300e2017e0b2eec26ea4e7
ssebide <sebiddejohn136@gmail.com>
2025-03-01 11:59:11 +0300

    Project setup

---
+++ backend/src/test/java/com/ssebide/Trading/TradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssebide.Trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
