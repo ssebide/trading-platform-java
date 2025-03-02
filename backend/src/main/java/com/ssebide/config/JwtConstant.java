075db04fb67d58c4adb25d98ec4305469f5e5184
ssebide <sebiddejohn136@gmail.com>
2025-03-02 22:27:01 +0300

    Implement Spring Security

---
+++ backend/src/main/java/com/ssebide/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.ssebide.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "sqhdgdtgqwdtahdvsatdfqhqgygsqtsqsqhaahawqsvwssbasa";
+    public static final String JWT_HEADER = "Authorization";
+}
--- backend/src/main/java/com/ssebide/controller/AuthController.java
+++ backend/src/main/java/com/ssebide/controller/AuthController.java
@@ -3,6 +3,9 @@ package com.ssebide.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,15 @@ public class AuthController {
     private UserRepository userRepository;
 
     @PostMapping("/signup")
-    public ResponseEntity<User> register( @RequestBody User user){
+    public ResponseEntity<User> register( @RequestBody User user) throws Exception{
+        
+
+        User isEmailExist = userRepository.findByEmail(user.getEmail());
+        
+        if (isEmailExist != null) {
+            throw new Exception("Email already exists!");
+        }
+
         User newUser = new User();
 
         newUser.setFullName(user.getFullName());
@@ -27,6 +38,10 @@ public class AuthController {
         newUser.setPassword(user.getPassword());
 
         User savedUser = userRepository.save(newUser);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
 
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
